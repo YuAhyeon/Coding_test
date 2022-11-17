5f12bd550b589b5b6f1c8df4880558ef6643407e
youa7878 <116181346+youa7878@users.noreply.github.com>
2022-11-18 00:16:54 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,17 @@
+function solution(s) {
+    
+    let str = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let num = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    
+    // 입력받은 문자열이 전부 숫자면 Number()로 리턴해 주고
+    // 입력받은 문자열이 전부가 숫자가 아닌 경우
+    // s에 replace를 해 준다  
+    while(isNaN(s)){
+        for(let i = 0 ; i < str.length ; i++){ // str의 요소를 순회
+            s = s.replace(str[i], num[i]); // s 문자열의 str의 i번째 요소를 num의 i번째 요소로 바꾼다.
+        }
+    } 
+    
+    return Number(s);
+    
+}
