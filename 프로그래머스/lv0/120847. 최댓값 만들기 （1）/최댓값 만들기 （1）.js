7691c94abbd305d196f6818855fc7a1834be3b76
Ahyeon <116181346+youa7878@users.noreply.github.com>
2023-06-06 12:17:01 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+  // 정렬 => pop() pop() 끼리 곱하기
+  
+  numbers = numbers.sort((a, b) => a - b)
+
+  const first = numbers.pop();
+  const second = numbers.pop();
+
+  return first * second
+}
+
+solution([0, 31, 24, 10, 1, 9]);
