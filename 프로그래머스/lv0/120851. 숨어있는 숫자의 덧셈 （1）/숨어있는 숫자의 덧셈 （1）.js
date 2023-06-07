efaed1027a7d3626711a753be1b635a3b0e9cf34
Ahyeon <116181346+youa7878@users.noreply.github.com>
2023-06-07 19:55:56 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -0,0 +1,13 @@
+function solution(my_string) {
+  // 정규식으로 문자열 제거, 배열로 변환 후 reduce로 합계 계산
+  const regex = /[a-zA-z]/g
+
+  return my_string
+    .replace(regex, '')
+    .split('')
+    .reduce((a, c) => Number(a) + Number(c));
+
+}
+
+solution("aAb1B2cC3zzz4oOp")
+solution("1a2b3c4d123")
