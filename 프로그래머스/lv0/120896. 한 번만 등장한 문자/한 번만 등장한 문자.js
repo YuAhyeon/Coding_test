34df6af98b18a9520fd25ca1057ba6deaef686ac
Ahyeon <116181346+youa7878@users.noreply.github.com>
2023-06-10 11:27:33 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -0,0 +1,13 @@
+function solution(s) {
+  s = [...s];
+  let result = {};
+
+  s.forEach((e) => {
+    !result.hasOwnProperty(e) ? result[e] = 1 : result[e] += 1;
+  })
+
+  result = Object.entries(result).sort((a, b) => a[1] - b[1])
+  return result.filter(e => e[1] < 2).map(e => e[0]).sort().join('');
+}
+
+solution("hello")
