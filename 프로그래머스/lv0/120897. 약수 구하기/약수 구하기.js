fbc71997c1b7d892b0f11ad8a8c3e6fbacabc09d
Ahyeon <116181346+youa7878@users.noreply.github.com>
2023-06-10 11:48:44 +0900

    [level 0] Title: 약수 구하기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
@@ -1,12 +1,12 @@
 function solution(n) {
-  const result = [1, ];
+  const result = [1];
 
   for(let i = 2; i <= n ; i++){
     if(n % i === 0){
       result.push(i);
     }
   }
-
+    
   return result;
 }
 
