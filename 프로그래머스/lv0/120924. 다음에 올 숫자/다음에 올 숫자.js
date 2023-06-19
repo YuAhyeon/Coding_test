f614a95fe77ff233e87e8d072e0d10063269bdf9
Ahyeon <116181346+youa7878@users.noreply.github.com>
2023-06-19 23:55:20 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -0,0 +1,9 @@
+function solution(common) {
+  const sameMinus = common[1] - common[0] === common[2] - common[1]
+
+  if(sameMinus) {
+      return common[common.length - 1] + common[1] - common[0]
+  } else {
+      return common[common.length - 1] * (common[1] / common[0])
+  }
+}
